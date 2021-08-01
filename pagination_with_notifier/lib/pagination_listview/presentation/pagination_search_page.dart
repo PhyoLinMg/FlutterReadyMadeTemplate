9a6cf8f8fbf330eb659b7b118dc4088043ae08b1
Lin Maung <gangsterplm@gmail.com>
2021-08-01 14:26:06 +0630

    yet to test but all code done

--- pagination_with_notifier/lib/pagination_listview/presentation/pagination_search_page.dart
+++ pagination_with_notifier/lib/pagination_listview/presentation/pagination_search_page.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pagination_with_notifier/pagination_listview/presentation/paginated_data_list_view.dart';
+import 'package:pagination_with_notifier/pagination_listview/shared/provider.dart';
 
-class PaginationSearchPage extends StatefulWidget {
+class PaginationSearchPage extends ConsumerStatefulWidget {
   const PaginationSearchPage({Key? key}) : super(key: key);
 
   @override
   _PaginationSearchPageState createState() => _PaginationSearchPageState();
 }
 
-class _PaginationSearchPageState extends State<PaginationSearchPage> {
+class _PaginationSearchPageState extends ConsumerState<PaginationSearchPage> {
   @override
   void initState() {
     super.initState();
@@ -16,7 +19,30 @@ class _PaginationSearchPageState extends State<PaginationSearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Column(
+        children: [
+          TextFormField(
+            onChanged: (value) {
+              ref.read(queryProvider).state = value;
+              Future.microtask(
+                () => ref
+                    .read(searchPaginationNotifierProvider.notifier)
+                    .getFirstSearchedDataPage(value),
+              );
+            },
+          ),
+          PaginatedDataListView(
+            paginatedDataNotifierProvider: searchPaginationNotifierProvider,
+            getNextPage: (/*ref*/ context) {
+              ref
+                  .read(searchPaginationNotifierProvider.notifier)
+                  .getNextSearchedDataPage(ref.read(queryProvider).state);
+            },
+            noResultsMessage:
+                "This is all we could find for your search term. Really...",
+          ),
+        ],
+      ),
     );
   }
 }
--- pagination_with_notifier/lib/pagination_listview/shared/provider.dart
+++ pagination_with_notifier/lib/pagination_listview/shared/provider.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pagination_with_notifier/pagination_listview/application/paginate_notifier.dart';
 import 'package:pagination_with_notifier/pagination_listview/application/search_pagination_notifier.dart';
 import 'package:pagination_with_notifier/pagination_listview/infrastructure/api_service.dart';
 import 'package:pagination_with_notifier/pagination_listview/infrastructure/search_pagination_repository.dart';
@@ -18,7 +19,10 @@ final searchPaginationRepositoryProvider = Provider(
   ),
 );
 
-final searchPaginationNotifierProvider = Provider(
+final queryProvider = StateProvider<String>((ref) => '');
+
+final searchPaginationNotifierProvider = StateNotifierProvider.autoDispose<
+    SearchPaginationNotifier, PaginationState>(
   (ref) => SearchPaginationNotifier(
     ref.watch(searchPaginationRepositoryProvider),
   ),
