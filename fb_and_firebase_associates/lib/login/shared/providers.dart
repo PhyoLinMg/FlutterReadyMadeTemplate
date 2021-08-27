d379514510ccf41bf83f53058a549f2eed18c4cc
Lin Maung <gangsterplm@gmail.com>
2021-08-27 20:03:49 +0630

    providers added

---
+++ fb_and_firebase_associates/lib/login/shared/providers.dart
@@ -0,0 +1,28 @@
+import 'package:fb_and_firebase_associates/login/core/infrastructure/login_repository.dart';
+import 'package:fb_and_firebase_associates/login/fb_login/infrastructure/fb_login_repository.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final googleSignInProvider = Provider(
+  (ref) => GoogleSignIn(
+    scopes: <String>[
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ],
+  ),
+);
+
+final facebookAuthProvider = Provider((ref) => FacebookAuth.instance);
+
+final googleLogInRepositoryProvider = Provider<LoginRepository>(
+  (ref) => FbLoginRepository(
+    ref.watch(facebookAuthProvider),
+  ),
+);
+
+final facebookLoginRepositoryProvider = Provider<LoginRepository>(
+  (ref) => FbLoginRepository(
+    ref.watch(facebookAuthProvider),
+  ),
+);
