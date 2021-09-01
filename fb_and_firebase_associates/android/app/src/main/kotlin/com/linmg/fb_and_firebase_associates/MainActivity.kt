005f786c2266533a21e28cb0f2cef13e73554728
Lin Maung <gangsterplm@gmail.com>
2021-09-01 15:59:47 +0630

    setting up google and facebook auths

--- fb_and_firebase_associates/android/app/src/main/kotlin/com/linmg/fb_and_firebase_associates/MainActivity.kt
+++ fb_and_firebase_associates/android/app/src/main/kotlin/com/linmg/fb_and_firebase_associates/MainActivity.kt
@@ -1,6 +1,45 @@
 package com.linmg.fb_and_firebase_associates
 
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.engine.plugins.util.GeneratedPluginRegister
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 class MainActivity: FlutterActivity() {
+
+    
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegister.registerGeneratedPlugins(flutterEngine!!)
+       printHashKey(this@MainActivity)
+    }
+
+    fun printHashKey(pContext: Context) {
+        try {
+            val info: PackageInfo = pContext.getPackageManager().getPackageInfo(pContext.getPackageName(), PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey = String(Base64.encode(md.digest(), 0))
+                Log.i("MainActivity", "printHashKey() Hash Key: $hashKey")
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e("MainActivity", "printHashKey()", e)
+        } catch (e: Exception) {
+            Log.e("MainActivity", "printHashKey()", e)
+        }
+    }
+
+
 }
+
+
